a4fdaa059e1cfbffef0582055429a0375501b893
Manan <78347709+ManannJoshi@users.noreply.github.com>
2026-08-02 03:10:33 +0530

    editing overLoadedFunctions

--- overloadedFunctions/main.cpp
+++ overloadedFunctions/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <string>
 
+void orderPTSubway(std::string chutney, std::string veggies);
+
 int main(){
 
+    std::string chutney;
+    std::string veggies;
 
+    std::cout << "Enter chutney >>> ";
+    std::getline(std::cin,chutney);
+
+    std::cout << "Enter veggie >>> ";
+    std::getline(std::cin, veggies);
+
+    orderPTSubway(chutney,veggies);
 
     return 0;
 }
 
-void orderPTSubway(){
-    std::cout << "Here is your paneer tikka subway";
-}
-
 void orderPTSubway(std::string chutney, std::string veggies){
-    if(chutney == "southwest chipotle"){
-        
+    if(chutney == "southwest chipotle" || veggies == "onion"|| veggies == "tomato"|| veggies == "lettuce"|| veggies == "salt and pepper"){
+        std::cout << "Here is your paneer tikka subway with " << chutney << " and " << veggies;
     }
-    std::cout << "Here is your paneer tikka subway with " << chutney << " and " << veggies;
+    else{
+        std::cout << "veggie or chutney is not recognised!";
+    }
 }
