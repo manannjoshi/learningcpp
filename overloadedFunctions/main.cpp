b2dcdc9a4902d8b5d86d9e0f8b9776516794cc5f
Manan <78347709+ManannJoshi@users.noreply.github.com>
2026-08-01 23:59:35 +0530

    overloadedFunctions

---
+++ overloadedFunctions/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+
+
+    return 0;
+}
+
+void orderPTSubway(){
+    std::cout << "Here is your paneer tikka subway";
+}
+
+void orderPTSubway(std::string chutney, std::string veggies){
+    if(chutney == "southwest chipotle"){
+        
+    }
+    std::cout << "Here is your paneer tikka subway with " << chutney << " and " << veggies;
+}
