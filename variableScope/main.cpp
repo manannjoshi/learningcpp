adaed17c0171f3143d9dfb09d2a8dfc18059a30d
Manan <78347709+ManannJoshi@users.noreply.github.com>
2026-08-06 07:17:51 +0530

    learnt variableScopes and file management system in c++

--- variableScope/main.cpp
+++ variableScope/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 
-int myNum = 2;
+int myNum = 1; //GLOBAL VARIABLE
 
 void printNum();
+void printNum2();
 
 int main(){
 
-    int myNum = 1;
+    int myNum = 2;
     std::cout << myNum;
     printNum();
-
+    printNum2();
     return 0;
 } 
 
 void printNum( ){
     int myNum = 3;
+    std::cout << myNum;
+}
+
+void printNum2(){
     std::cout << myNum;
 }
